ccd4cbee5cd6f238a50de19341ddb66de8fb883b
charles-co <charlesboy49@gmail.com>
2021-06-02 11:05:54 +0100

    updates

--- shop/signals.py
+++ shop/signals.py
@@ -4,20 +4,20 @@ from django.dispatch import receiver
 from rad.utils import unique_slug_generator
 from shop.models import Product, Trouser, Wavecap
 
-@receiver(post_save, sender=Trouser)
-def trouser_post_save_receiver(sender, instance, created, *args, **kwargs):
-        if created:
-                instance.slug = unique_slug_generator(instance)
-                Product.objects.create(item=instance, name=instance.name, 
-                                        price=instance.variants.first().price, available=instance.available, 
-                                        is_back=instance.is_back, is_discounted=instance.is_discounted,
-                                        is_bestseller=instance.is_bestseller, is_featured=instance.is_featured)
+# @receiver(post_save, sender=Trouser)
+# def trouser_post_save_receiver(sender, instance, created, *args, **kwargs):
+#         if created:
+#                 instance.slug = unique_slug_generator(instance)
+#                 Product.objects.create(item=instance, name=instance.name, 
+#                                         price=instance.variants.first().price, available=instance.available, 
+#                                         is_back=instance.is_back, is_discounted=instance.is_discounted,
+#                                         is_bestseller=instance.is_bestseller, is_featured=instance.is_featured)
 
-@receiver(post_save, sender=Wavecap)
-def wavecap_post_save_receiver(sender, instance, created, *args, **kwargs):
-        if created:
-                instance.slug = unique_slug_generator(instance)
-                Product.objects.create(item=instance, name=instance.name, 
-                                        price=instance.variants.first().price, available=instance.available, 
-                                        is_back=instance.is_back, is_discounted=instance.is_discounted,
-                                        is_bestseller=instance.is_bestseller, is_featured=instance.is_featured)
+# @receiver(post_save, sender=Wavecap)
+# def wavecap_post_save_receiver(sender, instance, created, *args, **kwargs):
+#         if created:
+#                 instance.slug = unique_slug_generator(instance)
+#                 Product.objects.create(item=instance, name=instance.name, 
+#                                         price=instance.variants.first().price, available=instance.available, 
+#                                         is_back=instance.is_back, is_discounted=instance.is_discounted,
+#                                         is_bestseller=instance.is_bestseller, is_featured=instance.is_featured)
